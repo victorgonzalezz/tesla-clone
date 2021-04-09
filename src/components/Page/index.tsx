d47b519372c3dfdb506119392cf6b0dd47b5c699
Victor <vitopcg@gmail.com>
2021-04-09 20:54:49 +0100

    commit

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 
+import { ModelsWrapper, ModelSection } from '../Model';
+import DefaultOvervayContent from '../DefaultOverlayContent'
+
 import { Container } from './styles';
 
  const Page: React.FC = () => {
   return (
     <Container>
-      <h1>Page</h1>
+      <ModelsWrapper>
+        <div>
+          {[
+            'Model One',
+            'Model Two',
+            'Model Three',
+            'Model Four',
+            'Model Five',
+            'Model Six',
+            'Model Seven',
+            ].map(modelName => (
+              <ModelSection
+              key={modelName}
+                className="colored"
+                modelName={modelName}
+                overlayNode={
+              <DefaultOvervayContent
+                label={modelName}
+                description="Order Online for Delivery"
+              />
+            }
+          />
+            ))}
+        </div>
+       
+      </ModelsWrapper>
     </Container>
   )
 }
-
 export default Page;
